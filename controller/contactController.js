c642cbf337398976c3df66712495b067a9fe8cd6
Lan_wifi <lanthoiba_k20@mtu.ac.in>
2024-08-29 20:32:49 +0530

    spelling correction

--- controller/contactController.js
+++ controller/contactController.js
@@ -4,7 +4,7 @@ const createContact = async (req, res) => {
     const contact = await contactModel.create(req.body);
     res.status(201).json({
       success: true,
-      message: "contact succesfully created",
+      message: "Contact succesfully created",
       contact,
     });
   } catch (err) {
