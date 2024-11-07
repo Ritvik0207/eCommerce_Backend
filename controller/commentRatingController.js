1277a3bd67ad0ed0281e88a32563b2fc310e6320
Lan_wifi <lanthoiba_k20@mtu.ac.in>
2024-11-07 12:22:11 +0530

    remove strictPopulate

--- controller/commentRatingController.js
+++ controller/commentRatingController.js
@@ -70,8 +70,8 @@ const getCommentRating = async (req, res) => {
   try {
     const commentRating = await commentRatingModel
       .find()
-      .populate({ path: "product", strictPopulate: true })
-      .populate({ path: "users", strictPopulate: true });
+      .populate("product")
+      .populate("users");
     res.status(200).json({
       success: true,
       message: "CommentRating succesfully fetch",
