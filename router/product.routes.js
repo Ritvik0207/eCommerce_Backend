cc07cb1ba1f7d53e23ab407840acac7c27507679
Ritvik0207 <koduriritvik0207@gmail.com>
2025-02-26 19:55:56 +0530

    Minor Change for ProductUpdate

--- router/product.routes.js
+++ router/product.routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const {
   createProduct,
   getAllProducts,
@@ -20,14 +20,14 @@ const {
   getFilteredProducts,
   getCarouselProducts,
   changeMarkupPercentage,
-} = require('../controller/productController');
-const multer = require('multer');
-const authenticateAdmin = require('../middlewares/authenticateAdmin');
-const { validateObjectId } = require('../middlewares/validateObjectId');
+} = require("../controller/productController");
+const multer = require("multer");
+const authenticateAdmin = require("../middlewares/authenticateAdmin");
+const { validateObjectId } = require("../middlewares/validateObjectId");
 
 // generating fields for uploading images in createProduct
 const generateVariantFields = (maxVariants = 10) => {
-  const fields = [{ name: 'baseImage', maxCount: 1 }];
+  const fields = [{ name: "baseImage", maxCount: 1 }];
 
   for (let i = 1; i <= maxVariants; i++) {
     fields.push({ name: `variant_${i}`, maxCount: 5 });
@@ -39,61 +39,59 @@ const router = express.Router();
 const upload = multer();
 //category router
 router.post(
-  '/create',
+  "/create",
   authenticateAdmin,
   upload.fields(generateVariantFields()),
   createProduct
 );
-router.get('/allproducts', getAllProducts);
-router.get('/carousel', getCarouselProducts);
-router.get('/getOneProduct/:id', getProductById);
+router.get("/allproducts", getAllProducts);
+router.get("/carousel", getCarouselProducts);
+router.get("/getOneProduct/:id", getProductById);
 router.put(
-  '/update/:id',
+  "/update/:id",
   authenticateAdmin,
-  validateObjectId,
-  upload.array('images'),
-  upload.single('baseImage'),
+  upload.fields(generateVariantFields()),
   updateProduct
 );
 router.delete(
-  '/delete/:id',
+  "/delete/:id",
   authenticateAdmin,
   validateObjectId,
   deleteProduct
 );
-router.get('/shop/:id', validateObjectId, getProductsByShopId);
-router.get('/seller/:id', validateObjectId, getProductsBySellerId);
+router.get("/shop/:id", validateObjectId, getProductsByShopId);
+router.get("/seller/:id", validateObjectId, getProductsBySellerId);
 router.get(
-  '/filterbyprice/:category/:subcategory?/:discountedPrice',
+  "/filterbyprice/:category/:subcategory?/:discountedPrice",
   filterByPrice
 );
 
-router.get('/filterbyprice/:new_price', filterAllProductByPrice);
-router.get('/products/:productId/comments', getProductWithComments);
-router.put('/updatefav/:id', updateProductFav);
-router.get('/query', getProductById);
-router.post('/createProductType', createProductTypes);
-router.get('/getproductType/:types', getProductTypes);
-router.get('/getProductsCategoryById', getProductsByCategoryId);
-router.get('/total', getTotalProductCount);
-router.get('/filterbyprice/:new_price', filterAllProductByPrice);
-router.get('/allproduct', getAllProducts);
-router.get('/products/:productId/comments', getProductWithComments);
-router.put('/update/:id', upload.array('image_id'), updateProduct);
-router.put('/updatefav/:id', updateProductFav);
-router.delete('/delete/:id', deleteProduct);
-router.get('/getOneProduct/:id', getProductById);
-router.get('/query', getProductById);
-router.post('/createProductType', createProductTypes);
-router.get('/getproductType/:types', getProductTypes);
-router.get('/getProductsCategoryById', getProductsByCategoryId);
-router.get('/total', getTotalProductCount);
+router.get("/filterbyprice/:new_price", filterAllProductByPrice);
+router.get("/products/:productId/comments", getProductWithComments);
+router.put("/updatefav/:id", updateProductFav);
+router.get("/query", getProductById);
+router.post("/createProductType", createProductTypes);
+router.get("/getproductType/:types", getProductTypes);
+router.get("/getProductsCategoryById", getProductsByCategoryId);
+router.get("/total", getTotalProductCount);
+router.get("/filterbyprice/:new_price", filterAllProductByPrice);
+router.get("/allproduct", getAllProducts);
+router.get("/products/:productId/comments", getProductWithComments);
+router.put("/update/:id", upload.array("image_id"), updateProduct);
+router.put("/updatefav/:id", updateProductFav);
+router.delete("/delete/:id", deleteProduct);
+router.get("/getOneProduct/:id", getProductById);
+router.get("/query", getProductById);
+router.post("/createProductType", createProductTypes);
+router.get("/getproductType/:types", getProductTypes);
+router.get("/getProductsCategoryById", getProductsByCategoryId);
+router.get("/total", getTotalProductCount);
 router.get(
-  '/sorted/:categoryId/:subcategoryId?',
+  "/sorted/:categoryId/:subcategoryId?",
   getCollectionNamesByCategoryAndSubcategory
 );
-router.get('/collectionfiltername', getFilteredProducts);
-router.put('/changeMarkupPercentage', changeMarkupPercentage);
+router.get("/collectionfiltername", getFilteredProducts);
+router.put("/changeMarkupPercentage", changeMarkupPercentage);
 
 // route.delete("/delete/:id", deleteCategory);
 // route.put("/update/:id", updateCategory);
